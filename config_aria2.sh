e51de8f282c4f2f616b130dd6595f121e1c3f191
doublesixwings <34424192+doublesixwings@users.noreply.github.com>
2020-09-04 05:00:01 +0800

    Add files via upload

---
+++ config_aria2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#配置aria2
+/etc/init.d/aria2 stop
+sed -i "s@/usr/local/caddy/www/aria2/Download@/mnt/blockstorage/www/html/aria2/download@" /root/.aria2/aria2.conf
+sed -i "s@peer-id-prefix=-TR2770-@#peer-id-prefix=-TR2770-@" /root/.aria2/aria2.conf
+sed -i "s@user-agent=Transmission/2.77@#user-agent=Transmission/2.77@" /root/.aria2/aria2.conf
+list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+if [ -z "`grep "bt-tracker" /root/.aria2/aria2.conf`" ]; then
+    sed -i '$a bt-tracker='${list} /root/.aria2/aria2.conf
+    echo add......
+else
+    sed -i "s@bt-tracker.*@bt-tracker=$list@g" /root/.aria2/aria2.conf
+    echo update......
+fi
+/etc/init.d/aria2 start
+ 
---
+++ others.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#附加存储块
+parted -s /dev/vdb mklabel gpt
+parted -s /dev/vdb unit mib mkpart primary 0% 100%
+mkfs.ext4 /dev/vdb1
+mkdir /mnt/blockstorage
+echo /dev/vdb1               /mnt/blockstorage       ext4    defaults,noatime,nofail 0 0 >> /etc/fstab
+mount /mnt/blockstorage
+
+#安装apache和yaaw
+mkdir -p /mnt/blockstorage/www/html/aria2/download
+yum -y install httpd
+cd /mnt/blockstorage/www/html
+git clone https://github.com/binux/yaaw
+
+#配置apache
+sed -i "s@/var/www/html@/mnt/blockstorage/www/html@" /root/.aria2/aria2.conf
+service httpd start
+
+#设置防火墙
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 80 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 80 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 51413 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 51413 -j ACCEPT
+service iptables save
+service iptables restart
+
+#安装魔改BBR
+yum -y install make gcc
+wget -O ./tcp_tsunami.c https://www.moerats.com/usr/down/CentOS_Kernel/tcp_tsunami.c
+echo "obj-m:=tcp_tsunami.o" > Makefile
+make -C /lib/modules/`uname -r`/build M=`pwd` modules CC=/usr/bin/gcc
+chmod +x ./tcp_tsunami.ko
+cp -rf ./tcp_tsunami.ko /lib/modules/`uname -r`/kernel/net/ipv4
+insmod tcp_tsunami.ko
+depmod -a
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=tsunami" >> /etc/sysctl.conf
+sysctl -p
