693f96bb8a3ab0db587827c7ff1f897948272031
doublesixwings <34424192+doublesixwings@users.noreply.github.com>
2020-09-04 08:22:08 +0800

    modify

--- others.sh
+++ others.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+yum -y install make gcc
+wget -O ./tcp_tsunami.c https://www.moerats.com/usr/down/CentOS_Kernel/tcp_tsunami.c
+echo "obj-m:=tcp_tsunami.o" > Makefile
+make -C /lib/modules/`uname -r`/build M=`pwd` modules CC=/usr/bin/gcc
+chmod +x ./tcp_tsunami.ko
+cp -rf ./tcp_tsunami.ko /lib/modules/`uname -r`/kernel/net/ipv4
+insmod tcp_tsunami.ko
+depmod -a
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=tsunami" >> /etc/sysctl.conf
+sysctl -p
+
 parted -s /dev/vdb mklabel gpt
 parted -s /dev/vdb unit mib mkpart primary 0% 100%
 mkfs.ext4 /dev/vdb1
@@ -18,14 +30,3 @@ iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 51413 -j ACCEPT
 service iptables save
 service iptables restart
 
-yum -y install make gcc
-wget -O ./tcp_tsunami.c https://www.moerats.com/usr/down/CentOS_Kernel/tcp_tsunami.c
-echo "obj-m:=tcp_tsunami.o" > Makefile
-make -C /lib/modules/`uname -r`/build M=`pwd` modules CC=/usr/bin/gcc
-chmod +x ./tcp_tsunami.ko
-cp -rf ./tcp_tsunami.ko /lib/modules/`uname -r`/kernel/net/ipv4
-insmod tcp_tsunami.ko
-depmod -a
-echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_congestion_control=tsunami" >> /etc/sysctl.conf
-sysctl -p
