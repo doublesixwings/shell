dd30b3407585dc0cafa6a2fb257fd30455556423
doublesixwings <34424192+doublesixwings@users.noreply.github.com>
2024-06-29 17:40:49 +0800

    Update others.sh

--- others.sh
+++ others.sh
@@ -11,16 +11,18 @@ echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=tsunami" >> /etc/sysctl.conf
 sysctl -p
 
-parted -s /dev/vdb mklabel gpt
-parted -s /dev/vdb unit mib mkpart primary 0% 100%
-mkfs.ext4 /dev/vdb1
-mkdir /mnt/blockstorage
-echo /dev/vdb1               /mnt/blockstorage       ext4    defaults,noatime,nofail 0 0 >> /etc/fstab
-mount /mnt/blockstorage
+#parted -s /dev/vdb mklabel gpt
+#parted -s /dev/vdb unit mib mkpart primary 0% 100%
+#mkfs.ext4 /dev/vdb1
+#mkdir /mnt/blockstorage
+#echo /dev/vdb1               /mnt/blockstorage       ext4    defaults,noatime,nofail 0 0 >> /etc/fstab
+#mount /mnt/blockstorage
 
-mkdir -p /mnt/blockstorage/www/html/aria2/download
 yum -y install httpd
-cd /mnt/blockstorage/www/html
+#mkdir -p /mnt/blockstorage/www/html/aria2/download
+mkdir -p /www/html/aria2/download
+#cd /mnt/blockstorage/www/html
+cd /www/html
 git clone https://github.com/binux/yaaw
 
 #iptables -I INPUT -m state --state NEW -m udp -p udp --dport 80 -j ACCEPT
@@ -30,10 +32,7 @@ git clone https://github.com/binux/yaaw
 #service iptables save
 #service iptables restart
 firewall-cmd --zone=public --add-port=80/tcp --permanent
-firewall-cmd --zone=public --add-port=80/udp --permanent
 firewall-cmd --zone=public --add-port=51413/tcp --permanent
 firewall-cmd --zone=public --add-port=51413/udp --permanent
 firewall-cmd --zone=public --add-port=6800/tcp --permanent
-firewall-cmd --zone=public --add-port=6800/udp --permanent
 firewall-cmd --reload
-
